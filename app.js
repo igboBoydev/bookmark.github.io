e586df01987d8e41493a3570b72ee62976323d78
xtremeCode <abelkelly6022@gmail.com>
2021-04-15 23:28:57 +0100

    uploaded items to repository

---
+++ app.js
@@ -0,0 +1,45 @@
+const harmburger = document.querySelector('.image1');
+const closeIcon = document.querySelector('.image2');
+const img = document.querySelectorAll('.img');
+const img1 = document.querySelectorAll('.img1');
+const p = document.querySelectorAll('.par')
+harmburger.addEventListener('click', function(){
+    harmburger.classList.add('display1');
+    harmburger.classList.remove('active')
+    closeIcon.classList.add('active');
+    closeIcon.classList.remove('display1');
+})
+
+closeIcon.addEventListener('click', function(){
+    harmburger.classList.remove('display1');
+    harmburger.classList.add('active');
+    closeIcon.classList.add('display1');
+    closeIcon.classList.remove('active');
+})
+
+for(let i = 0; i < img.length; i++){
+    img[i].addEventListener('click', () => {
+        img[i].classList.add('display1')
+        for(let j = 0; j < img1.length; j++){
+            img1[i].classList.remove('display1')
+        }
+        for(let k = 0; k < p.length; k++){
+            p[i].classList.remove('display1')
+            p[i].classList.add('align')
+        }
+    })
+}
+
+for(let i = 0; i < img1.length; i++){
+    img1[i].addEventListener('click', () => {
+        img1[i].classList.add('display1')
+        for(let j = 0; j < img.length; j++){
+            img[i].classList.remove('display1')
+        }
+        for(let k = 0; k < p.length; k++){
+            p[i].classList.add('display1')
+            p[i].classList.remove('align')
+        }
+    })
+}
+
